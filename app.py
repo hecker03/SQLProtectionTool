edd9b128583795c82b274c0bad9cb18323a07eba
Shrishailya Abhay Keskar <keskarshrishailya@gmail.com>
2025-01-21 19:12:16 +0530

    Idenitfy Programming lang. and vulnerable query

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import openai
 import mysql.connector
+import openai
+import re
 
 base_url = "https://api.aimlapi.com/v1"
 api_key = "4e18cfb70cae4c1e8caeecf8fd97ffee"
@@ -24,3 +25,26 @@ def get_response(usrprompt, model="mistralai/Mistral-7B-Instruct-v0.2"):
         return completion['choices'][0]['message']['content']
     except Exception as e:
         return f"An error occurred: {e}"
+    
+# Identify the programming language and vulnerable query
+def get_proglang_by_code(source_code):
+    lines = source_code.splitlines()
+    prompt = ""
+    vulnerable_query = ""
+    db = connect_to_database()
+    cur = db.cursor(dictionary=True)
+    q = "Select * from identify"
+    cur.execute(q)
+    res = cur.fetchall()
+    for x in res:
+        for line in lines:
+            if x[2] in line:
+                prompt = line
+            else:
+                pattern = r"SELECT\s+.*?\s+FROM\s+\w+"
+                matches = re.findall(pattern, line)
+                if matches:
+                    vulnerable_query = line
+    if prompt:
+        prompt += "\nWhich language is this?(one-word answer)"
+    return get_response(prompt), vulnerable_query
